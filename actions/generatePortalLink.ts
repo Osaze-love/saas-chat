3e61ad3624b00c000e98a43fcbb101a0a729ce40
Osaze-Omobude Love <loveomobude23@gmail.com>
2024-06-04 17:03:40 +0100

    stripe apiversion updated

--- actions/generatePortalLink.ts
+++ actions/generatePortalLink.ts
@@ -7,7 +7,7 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-04-10",
 });
 
 export const generatePortalLink = async () => {
