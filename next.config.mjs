1c9bbf3e3ad6b4e1af2e2fa6e48411ce068a7308
Osaze-Omobude Love <loveomobude23@gmail.com>
2024-06-04 17:26:22 +0100

    change made

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,22 @@ const nextConfig = {
   images: {
     domains: ["github.com", "lh3.googleusercontent.com"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
 
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 export default nextConfig;
